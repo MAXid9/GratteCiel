54ac362198985806fbbfae96b51e30111572e630
maxid9 <maxence.hamel9@gmail.com>
2018-11-28 22:41:05 +0100

    Création des deux premiers prédicats gratteCiel/2 qui permet de lancer un problème en indiquant le nombre de gratte ciels par coté

--- gratteCielHamel.pl
+++ gratteCielHamel.pl
@@ -2,3 +2,9 @@
  * Hamel Maxence
  */
 
+% On regarde si la partie qu'on lance est bien instanciée ou non
+	% Si elle ne l'est pas on le notifie à l'utilisateur
+gratteCiel(NbCote):-NbCote =< 0, writeln('Mauvaise saisie !'),!.
+	% Si elle l'est j'initialise un plateau de jeu aux bonnes
+	% dimensions.
+gratteCiel(NbCote):-initialise(NbCote).
